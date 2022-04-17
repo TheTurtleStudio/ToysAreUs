079c7ba0da374459563f4446c9e1c64ac626655d
Turtle <braydenlynnpeterson@gmail.com>
2022-04-17 17:58:50 -0600

    Removed code
    Forgot to remove functionality for the generic GameObject class. Debugging stuff that we don’t want in there.

--- src/GameObjects/GameObject.py
+++ src/GameObjects/GameObject.py
@@ -7,16 +7,10 @@ class GameObject(): #Change this to the name of your script
         self.Start()
 
     def Start(self): #Called when the object is added to the scene.
-        self.speed = 20
-        self.sizeIncrease = 0
+        pass
 
     def Update(self): #This is called every rendercycle
-        #Below is some example code that moves the square. The attached GameObject.
-        left = -1 if self.engine.Input.TestFor.KEYDOWN(pygame.K_a) else 0
-        right = 1 if self.engine.Input.TestFor.KEYDOWN(pygame.K_d) else 0
-        up = -1 if self.engine.Input.TestFor.KEYDOWN(pygame.K_w) else 0
-        down = 1 if self.engine.Input.TestFor.KEYDOWN(pygame.K_s) else 0
-        self.gameObject.position += self.speed * Types.Vector3((left+right), (up+down), 0) * self.engine.GetDeltaTime()
+        pass
 #Create needs to be defined for every script in this folder. Everything should be exactly the same except for what is commented below, read that.
 class Create():
     def __init__(self, engine):
